aca882aaf8a45edfd0b39b194e6843e0e1a69486
alanniedecena <alannie.khem.decena@gmail.com>
2023-02-23 21:23:47 +1300

    examples

--- jsExamples.js
+++ jsExamples.js
@@ -77,3 +77,77 @@
 //         this.firstname = firstnameValue;
 //         this.lastname = lastnameValue;
 //     }
+//to fix
+// let pi = 3.14569
+// let amount = 25.33333
+
+// console.log(pi.toFixed(3))
+// console.log(parseFloat(amount.toFixed(2)) + 10.50)
+//parseInt
+//isInteger and parseFloat
+//isInteger and isNan
+
+// let quickFox = 'he a quick fox';
+// let quickFoxUpper = quickFox.toUpperCase(); 
+
+// console.log(quickFox.toUpperCase);
+// console.log(quickFox.toLowerCase);
+
+// function compareString(string1, string2) {
+//     return string1.toUpperCase() == string2.toUpperCase()
+// }
+// console.log(compareString(quickFox, quickFoxUpper))
+// console.log(quickFox.startsWith('He'))
+
+// let name1 = 'Adam'
+// let name2 = 'Alannie'
+// let name3 = 'Robin'
+
+// function startWithA(name) {
+//     return name.toUpperCase().startWith('A')
+
+// }
+// console.log(name1+ `starts with a? ` +startWithA(name1))
+
+// let longString = "this is fairly long string with lot sof different characters in it.";
+// let shortString = "short string";
+ 
+// function truncate (fullString, maxChars) {
+//     return fullString.subString(0, maxChars)
+
+// }
+// console.log(truncate(longString, 10))
+
+// console.log(truncate(shortString, 10))
+
+//split Function
+
+let longString = "this is fairly long string with lot sof different characters in it.";
+let shortString = "short string";
+
+// let wordsplit = longString.split(' ')
+
+// console.log(longString)
+
+
+// function countWords(string) {
+//     let words = longString.split(' ')
+//     console.log(words)
+//     return words.lenght;
+
+// }
+
+// console.log('there arre' +countWords(longString) + 'words in' + longString)
+// console.log('there arre' +countWords(longString) + 'words in' + longString)
+
+// const user = {
+//     name: 'kim',
+//     location: 'akl',
+//     toString() {
+//         return `${this.name} is from ${this.location}`
+//     }
+
+// }
+
+// console.log('this person is ' + user);
+
