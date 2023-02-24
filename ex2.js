59d26724ce65beb8a059383c30eba9a87059067d
alanniedecena <alannie.khem.decena@gmail.com>
2023-02-25 11:04:38 +1300

    ex2.js

---
+++ ex2.js
@@ -0,0 +1,19 @@
+// function ucFirstLetters (string) {
+//     return string = string.charAt(0).toUpperCase() + string.slice(1);
+// }
+
+// console.log(ucFirstLetters("japan") );
+// END OF  QUESTION 1
+
+let longString = 'this is a verrrryyyyyyyyyyyy lonnnnnnnnnnnng strinnnnnnnnnnnng';
+
+let shortString = 'she a short string'
+
+function truncates (str, max) {
+   
+     if ([...str].length )
+    
+  }
+  
+  
+console.log(shortString)
--- jsExamples.js
+++ jsExamples.js
@@ -122,8 +122,8 @@
 
 //split Function
 
-let longString = "this is fairly long string with lot sof different characters in it.";
-let shortString = "short string";
+// let longString = "this is fairly long string with lot sof different characters in it.";
+// let shortString = "short string";
 
 // let wordsplit = longString.split(' ')
 
@@ -151,3 +151,91 @@ let shortString = "short string";
 
 // console.log('this person is ' + user);
 
+// const product = {
+//     name : "company name",
+//     category: "useful thing",
+//     price: 123,
+//     discount: 10,
+//     valueof() {
+
+//         return this.price = (this.price * (this.discount/100));
+//     },
+
+//     toString() {
+//         return `${this.name} ${this.category}`
+//     }
+    
+// }
+
+// const product2 = {
+//     name : "company nameA",
+//     category: "useful thing2",
+//     price: 15.00,
+//     discount: 10,
+//     valueof() {
+
+//         return this.price - (this.price * (this.discount/100));
+//     },
+
+//     toString() {
+//         return `${this.name} ${this.category}`
+//     }
+    
+// }
+// console.log(`${product2} with quantiy 5 cost ${(product * 5)}`);
+
+// let billion = 1_000_000_000;
+// console.log(billion);
+
+const cities_au = ['Sydney', 'Melbourne', 'Adelaide', 'Cairns', 'Brisbane']
+const cities_nz = ['Auckland', 'Christchurch', 'Hamilton', 'Wellington', 'Queenstown']
+
+// cities_au.push("perth")
+// cities_au.unshift("Adelaide")
+
+// lastCity = cities_nz.pop('Queenstown')
+//  const cities = cities_au.concat(cities_nz)
+
+// console.log(cities_au)
+// console.log(cities_nz)
+// console.log(cities)
+
+// cities_nz.splice(4, 0, 'Taupo')
+// console.log(cities_nz)
+// cities_nz.forEach(cities => console.log(cities));
+
+// const pepole = [
+//     {firstName: 'A', lastName: '1', age: 12}
+//     {firstName: 'B', lastName: '2', age: 13}
+//     {firstName: 'C', lastName: '3', age: 14}
+//     {firstName: 'D', lastName: '4', age: 15}
+// ]
+
+// pepole.forEach(person => console.log(`${person.firstName} ${person.lastName} is ${person.age} years old`))
+
+// const people = [
+//     {firstName: 'Wendy', lastName: 'Darton', age: 52, location: 'QLD'},
+//     {firstName: 'Steven', lastName: 'Williams', age: 41, location: 'NSW'},
+//     {firstName: 'Phillip', lastName: 'Robertson', age: 38, location: 'VIC'},
+//     {firstName: 'Connor', lastName: 'Edmonds', age: 24, location: 'NZ'},
+//     {firstName: 'Sheila', lastName: 'Horne', age: 37, location: 'QLD'},
+// ]
+// //  people.forEach(person => {
+// //     console.log(person.location);
+  
+// //  })
+//  let vicPerson = people.find(person => person.location == 'VIC') //.find()
+// console.log(vicPerson)
+// let sPerson = people.filter(person => person.firstName.startsWith('S'))//.filter()
+// console.log(sPerson)
+
+// let newAges = people.map(person => person.age + 1)//.map()
+// console.log(newAges)
+
+// people.sort((person1, person2) => {   //.sort()
+//     if (person1.firstName == person2.firstName) return 0;
+//     if (person1.firstName > person2.firstName) return 1;
+//     return -1;
+// });
+
+// console.log(people);
