4d74bc8cea2bd74a0822e56c00cb59200bee3baa
alanniedecena <alannie.khem.decena@gmail.com>
2023-03-06 19:15:50 +1300

    X_X

--- ex3.js
+++ ex3.js
@@ -52,3 +52,104 @@
 // setTimeout( printMe, 300, ' msg 3');
 // END OF QUESTION 3
 
+// function printFibonacci(n) {
+//     let a = 0;
+//     let b = 1;
+    
+//     console.log(a)
+//     console.log(b)
+ 
+//     setInterval(function printNext() {
+//         let c = a + b
+//         console.log(c)
+//         a = b;
+//         b = c;
+//     },1000 )
+// } 
+
+// printFibonacci()
+
+// function printFibonacciB(n) {
+//     let  [a, b ] = [1, 1]
+    
+//     console.log(a)
+//     console.log(b)
+ 
+//     setTimeout(function printNext(a, b) {
+//         let c = a + b
+//         console.log(c)
+//         setTimeout(printNext, 1000, b, c) 
+//     },1000, a, b)
+// } 
+// printFibonacciB()
+
+// function printFibonacciC(n) {
+//     let  [a, b ] = [1, 1]
+//     let counter = 2;
+//     console.log(a)
+//     console.log(b)
+ 
+//     setTimeout(function printNext(a, b) {
+//         let c = a + b
+//         counter = counter + 1;
+//         console.log(c)
+//         if (counter < n ){
+//         setTimeout(printNext, 1000, b, c) }
+//     },1000, a, b)
+// } 
+// printFibonacciC(10)
+// END OF QUESTION 4
+
+
+// let car = {
+//     make: "Porsche",
+//     model: '911',
+//     year: 1964,
+// setTimeout(description) {console.log(`This car is a ${this.make} ${this.model} from ${this.year}`); 
+// 200}
+// };
+
+// let car = {
+//     make: "Porsche",
+//     model: '911',
+//     year: 1964,
+//     description() {
+    
+//     console.log(`This car is a ${this.make} ${this.model} from ${this.year}`);
+//     }
+//     };
+
+// // setTimeout(car.description, 200); //fails //because it cant access the object
+
+// // setTimeout(() => car.description(), 1000)// A
+// // car = {...car, year:'2023' } // B
+// // C = no beacause we override it
+// let noWrapper = car.description.bind(car)
+// setTimeout(noWrapper, 2000) //D
+// car = {...car, model:'123' } //E
+// END OF QUESTION 5
+
+
+// function multiply(a, b) {
+//     console.log( a * b );
+//  }
+
+// delay.prototype = setTimeseout() 
+
+
+// multiply.delay(500)(5, 5);
+// END OF QUESTION 6 ??
+
+function Person(name, age, gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+ }
+
+const cusPerson = new Person['Kent', 23, 'non-binary']
+
+cusPerson.toString()
+
+const arr = [1, 2, 3];
+
+console.log(cusPerson.toString())
